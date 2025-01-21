4b32e9fdfdc156d50498165527ae28d473d63c5b
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2025-01-21 16:52:11 +0100

    30. Defining Component Inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  @Input() avatar!: string;
+  @Input() name!: string;
+  
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
   onSelectUser() {
   }
 }
