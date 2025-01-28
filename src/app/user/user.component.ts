f01d47ce0b05f9592df392bd54cd1c3d205694b0
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2025-01-28 19:17:40 +0100

    35. Using the output() Function

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, input, Output, EventEmitter, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,7 +10,8 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   avatar = input.required<string>();
   name = input.required<string>();
-  @Output() select = new EventEmitter();
+  // @Output() select = new EventEmitter();
+  select = output<string>();
 
   imagePath = (() => {
     return 'assets/users/' + this.avatar();
