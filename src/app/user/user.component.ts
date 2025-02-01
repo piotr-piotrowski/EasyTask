7a101ef9befc175d005d3402bdb1a4946e6c4166
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2025-02-01 21:08:28 +0100

    48. Dynamic CSS Styling with Class Bindings

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,6 +10,7 @@ import { User } from './user.model';
 })
 export class UserComponent {
   @Input({required: true}) user!: User; 
+  @Input({required: true}) selected!: boolean;
 
   select = output<string>();
 
