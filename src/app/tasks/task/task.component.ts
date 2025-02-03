d46b50f020de36de64646ea92f9d41a0eb2c1e6b
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2025-02-03 20:18:09 +0100

    56. Content Projection with ng-content

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,18 +1,19 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { Task } from './task.model';
+import { CardComponent } from '../../shared/card/card.component';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
   @Input({required: true}) task!: Task; 
   @Output() complete = new EventEmitter<string>();
-  
+
   onCompleteTask() {
     this.complete.emit(this.task.id);
   }
