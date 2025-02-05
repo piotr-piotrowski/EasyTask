30846ea553d18672d9da5c2da5d19120daed5c9d
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2025-02-05 14:56:24 +0100

    67. Bootstraping Apps with Angular Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
+
 import { DUMMY_USERS } from './dummy-users';
-import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
